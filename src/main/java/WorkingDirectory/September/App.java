c1bbd8826add6ab5fe21971f9393ef09d49d460a
AntonZ <gexanton20@gmail.com>
2019-09-17 16:09:39 +0300

    @@2@@

--- src/main/java/WorkingDirectory/September/App.java
+++ src/main/java/WorkingDirectory/September/App.java
@@ -3,5 +3,6 @@ package WorkingDirectory.September;
 public class App {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
+        System.out.println("ItsAlive!");
     }
 }
